60388b80e41c3af6d42fa40fe9893532cb57480c
LordGuenni <florian.stamer@outlook.de>
2024-05-21 15:12:52 +0200

    Added Multiplayer, Refactored Code

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 const colorDisplays = document.querySelectorAll('.color-display');
 const colorLists = document.querySelectorAll('.color-list');
 const colorOptions = document.querySelectorAll('.color-option');
-
-colorDisplays.forEach((display, index) => {
-    display.addEventListener('click', () => {
-        colorLists[index].classList.toggle('show');
-    });
-});
-
 const colors = ['red', 'blue', 'green', 'yellow', 'brown'];
-const secretCode = Array.from({ length: 4 }, () => colors[Math.floor(Math.random() * colors.length)]);
-console.log('Secret Code:', secretCode);
+const submitButton = document.getElementById('submit-guess');
 
+
+
+let secretCodeSet = false;
+let secretCode = new Array(4);
 let guess = new Array(4);
 
-colorOptions.forEach(option => {
-    option.addEventListener('click', () => {
-        const color = option.dataset.color;
-        if (color) {
-            const colorDisplay = option.parentNode.previousElementSibling;
-            const index = Array.from(colorDisplays).indexOf(colorDisplay);
-            guess[index] = color;
-            colorDisplay.style.backgroundColor = color;
-            option.parentNode.classList.toggle('show');
-        }
-    });
-});
+// Allow Player 1 to set the Secret Code
+if (!secretCodeSet) {
+    submitButton.textContent = 'Player 1 Enter Code Please';
+}
 
 function calculateGuessResult(guess, correctAnswer) {
     let white = 0;
@@ -54,60 +42,96 @@ function calculateGuessResult(guess, correctAnswer) {
     return { white, black };
 }
 
+colorDisplays.forEach((display, index) => {
+    display.addEventListener('click', () => {
+        colorLists[index].classList.toggle('show');
+    });
+});
+
+// random gen the SecretCode (Old Code)
+// const secretCode = Array.from({ length: 4 }, () => colors[Math.floor(Math.random() * colors.length)]);
+
+colorOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        const color = option.dataset.color;
+        if (color) {
+            const colorDisplay = option.parentNode.previousElementSibling;
+            const index = Array.from(colorDisplays).indexOf(colorDisplay);
+            guess[index] = color;
+            colorDisplay.style.backgroundColor = color;
+            option.parentNode.classList.toggle('show');
+        }
+    });
+});
+
+// Guess Button Logic
 document.getElementById('submit-guess').addEventListener('click', function () {
     if (guess.includes(undefined)) {
         alert('Please select 4 colors.');
         return;
     }
 
-    const result = calculateGuessResult(guess, secretCode);
-    const guessRow = document.createElement('div');
-    guessRow.className = 'guess-row';
-
-    const indexNumber = document.createElement('div');
-    indexNumber.className = 'index-number';
-    indexNumber.textContent = document.getElementsByClassName('guess-row').length + 1;
-    guessRow.appendChild(indexNumber);
-
-    guess.forEach(color => {
-        const guessItem = document.createElement('div');
-        guessItem.className = 'guess-item';
-        guessItem.dataset.color = color;
-        guessRow.appendChild(guessItem);
-    });
-
-    const guessResult = document.createElement('div');
-    guessResult.className = 'guess-result';
-
-    for (let i = 0; i < result.black; i++) {
-        const resultItem = document.createElement('div');
-        resultItem.className = 'guess-result-item';
-        resultItem.setAttribute('data-color', 'black');
-        guessResult.appendChild(resultItem);
+    if (secretCodeSet == false){
+        secretCode = guess
+        console.log(secretCode)
+        guess = new Array(4);
+        colorDisplays.forEach(display => {
+            display.style.backgroundColor = '';
+        });
+        alert('Code has been set, Player 2 can play now !');
+        secretCodeSet = true;
+        submitButton.textContent = 'Submit Guess';
     }
 
-    for (let i = 0; i < result.white; i++) {
-        const resultItem = document.createElement('div');
-        resultItem.className = 'guess-result-item';
-        resultItem.setAttribute('data-color', 'lavender');
-        guessResult.appendChild(resultItem);
-    }
+    else {
+        const result = calculateGuessResult(guess, secretCode);
+        const guessRow = document.createElement('div');
+        guessRow.className = 'guess-row';
 
-    guessRow.appendChild(guessResult);
-    document.getElementById('guesses').appendChild(guessRow);
+        const indexNumber = document.createElement('div');
+        indexNumber.className = 'index-number';
+        indexNumber.textContent = document.getElementsByClassName('guess-row').length + 1;
+        guessRow.appendChild(indexNumber);
 
-    if (result.black === guess.length) {
-        if (confirm('Success! You guessed the correct combination. Would you like to play again?')) {
-            location.reload();
+        guess.forEach(color => {
+            const guessItem = document.createElement('div');
+            guessItem.className = 'guess-item';
+            guessItem.dataset.color = color;
+            guessRow.appendChild(guessItem);
+        });
+
+        const guessResult = document.createElement('div');
+        guessResult.className = 'guess-result';
+
+        for (let i = 0; i < result.black; i++) {
+            const resultItem = document.createElement('div');
+            resultItem.className = 'guess-result-item';
+            resultItem.setAttribute('data-color', 'black');
+            guessResult.appendChild(resultItem);
         }
-    } else {
-        console.log('Failure. Please try again.');
-        console.log('Black pegs: ' + result.black);
-        console.log('White pegs: ' + result.white);
-    }
 
-    guess = new Array(4);
-    colorDisplays.forEach(display => {
-        display.style.backgroundColor = '';
+        for (let i = 0; i < result.white; i++) {
+            const resultItem = document.createElement('div');
+            resultItem.className = 'guess-result-item';
+            resultItem.setAttribute('data-color', 'lavender');
+            guessResult.appendChild(resultItem);
+        }
+
+        guessRow.appendChild(guessResult);
+        document.getElementById('guesses').appendChild(guessRow);
+
+        if (result.black === guess.length) {
+            if (confirm('Success! You guessed the correct combination. Would you like to play again?')) {
+                location.reload();
+            }
+        } else {
+            console.log('Failure. Please try again.');
+            console.log('Black pegs: ' + result.black);
+            console.log('White pegs: ' + result.white);
+        }
+        guess = new Array(4);
+        colorDisplays.forEach(display => {
+            display.style.backgroundColor = '';
+        });
+        }
     });
-});
